f1c3c9617416d4eec6b00026f4c7ab0e4b617d6e
venusekkeri <venusekkeri@gmail.com>
2020-11-30 12:41:49 +0530

    Update wel.py

--- wel.py
+++ wel.py
@@ -1,7 +1,21 @@
 import tkinter as tk
-master = tk.Tk()
-whatever_you_do = "Whatever you do will be insignificant, but it is very important that you do it.\n(Mahatma Gandhi)"
-msg = tk.Message(master, text = whatever_you_do)
-msg.config(bg='lightgreen', font=('times', 24, 'italic'))
-msg.pack()
-tk.mainloop()
+    
+
+def write_slogan():
+    print("Tkinter is easy to use!")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Hello",
+                   command=write_slogan)
+slogan.pack(side=tk.LEFT)
+
+root.mainloop()
