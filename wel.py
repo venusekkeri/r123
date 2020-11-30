a87b0e79d02ab1bb773575e8f0110a3763f1fe1c
venusekkeri <venusekkeri@gmail.com>
2020-11-30 13:14:46 +0530

    Update wel.py

--- wel.py
+++ wel.py
@@ -1,21 +1,12 @@
 import tkinter as tk
-    
 
-def write_slogan():
-    print("Tkinter is easy to use!")
+# if you are still working under a Python 2 version, 
+# comment out the previous line and uncomment the following line
+# import Tkinter as tk
 
 root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
 
-button = tk.Button(frame, 
-                   text="QUIT", 
-                   fg="red",
-                   command=quit)
-button.pack(side=tk.LEFT)
-slogan = tk.Button(frame,
-                   text="Hello",
-                   command=write_slogan)
-slogan.pack(side=tk.LEFT)
+w = tk.Label(root, text="Hello Tkinter!")
+w.pack()
 
 root.mainloop()
